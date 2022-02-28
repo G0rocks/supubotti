5885953459ea3777bde95569be5cfeed4395e529
Huldar <huldarh@gmail.com>
2022-02-28 00:00:18 +0000

    Add temp sensor

--- botti.py
+++ botti.py
@@ -1,10 +1,18 @@
 # Imports
+import time
 from time import sleep
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)  # Mode, til ad nota pinoutid sem vid erum ad nota
+from machine import Pin
+from onewire import DS18X20
+from onewire import OneWire
 
 # Constants
 STEP_SIZE= 1.8 # 1.8 Degrees but in microstepping
+MAX_SOUP_TEMP = 40
+
+# Variables
+soup_type = "Aspas"
 
 # Motor klassi
 class Motor:
@@ -43,8 +51,6 @@ class Motor:
       self.step()
       sleep(1/self.speed)
 
-
-
 # Setup
   # Motor 1
 pos1 = 0
@@ -63,8 +69,17 @@ mot2 = Motor(2, pos2, DIR2_PIN, STEP2_PIN, dir2, speed2)
 # Motors
 motors = [mot1, mot2]
   # TempSensor
+soup_temp = 100
+TEMP_PIN = 'P10'
+temp_sensor = DS18X20(OneWire(Pin(TEMP_PIN)))
 
-
+# Maela hita function
+def read_temp()
+  temper = temp.read_temp_async()
+  sleep(1)
+  temp.start_conversion()
+  sleep(1)
+  return temper
 
 
 # Fara i nullstodu
@@ -74,8 +89,25 @@ motors = [mot1, mot2]
 
 # Snua einn hring
 try:
+  # Nidur ad supu
   motors[1].to_deg(36)
   motors[0].to_deg(18)
+
+  # Greina supu
+  print("Súpa: " + soup_type)
+
+  # Maela supu
+  while (soup_temp > MAX_SOUP_TEMP):
+    # Blaka
+    motors[1].to_deg(-9)
+    motors[1].to_deg(9)
+    # Maela
+    soup_temp = read_temp()
+    
+
+    # Prenta
+    print("Soup temp: " + soup_temp)
+  
   sleep(2)
   motors[1].to_deg(-36)
   sleep(1)
